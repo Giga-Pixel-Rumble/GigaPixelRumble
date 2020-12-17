6886231ed620e80220bdc44c7d1fcd26f427b488
Jack Boswell (boswelja) <boswelja@outlook.com>
2020-12-17 17:49:21 +1300

    Add Android project

---
+++ GigaPixelRumble.Android/MainActivity.cs
@@ -0,0 +1,12 @@
+﻿using Android.App;
+using GigaPixelRumble.Game;
+using osu.Framework.Android;
+
+namespace GigaPixelRumble.Android
+{
+    [Activity(Label = "@string/app_name", Theme = "@android:style/Theme.NoTitleBar", MainLauncher = true)]
+    public class MainActivity : AndroidGameActivity
+    {
+        protected override osu.Framework.Game CreateGame() => new GigaPixelRumbleGame();
+    }
+}
