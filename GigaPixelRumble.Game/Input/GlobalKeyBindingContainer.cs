ce149a9cee0e4642567bd0f17dcbb8c05d11e8a8
Jack Boswell (boswelja) <boswelja@outlook.com>
2020-12-17 22:24:42 +1300

    Create some KeyBindingContainers One for basic player input, and one for menu navigation

---
+++ GigaPixelRumble.Game/Input/GlobalKeyBindingContainer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using osu.Framework.Input.Bindings;
+
+namespace GigaPixelRumble.Game.Input
+{
+    public enum GlobalAction
+    {
+        Back,
+        Click
+    }
+
+    public class GlobalKeyBindingContainer : KeyBindingContainer<GlobalAction>
+    {
+        public override IEnumerable<KeyBinding> DefaultKeyBindings => new[]
+        {
+            new KeyBinding(new[] { InputKey.Escape }, GlobalAction.Back),
+            new KeyBinding(new[] { InputKey.MouseLeft }, GlobalAction.Click)
+        };
+    }
+}
